2d3bc4c0589023233fcba185b5bfbd4d1c9061b1
PavelDusek <pavel.dusek@gmail.com>
2023-01-31 02:54:52 +0100

    list of wifis, display notification, try to set up time every hour

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ import ujson
 import urequests
 
 url = "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Prague"
-ssid, key = "", ""
+wifi_list = [
+    #enter list of WLANs to try:
+    ("<ssid1>", "<key1>"),
+    ("<ssid2>", "<key2>"),
+    ("<ssid3>", "<key3>"),
+]
+ssid, key = None, None
+wifi_wait = 10
 
-def do_connect(ssid, key):
+def try_connect(ssid, key):
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('connecting to network {} with key {}'.format(ssid, key))
         sta_if.active(True)
         sta_if.connect(ssid, key)
-        while not sta_if.isconnected():
-            pass
+        if not sta_if.isconnected():
+            time.sleep(wifi_wait)
     print('network config:', sta_if.ifconfig())
+    return sta_if.isconnected()
 
 def measure():
     sensor = dht.DHT11(machine.Pin(2))
@@ -27,37 +35,82 @@ def measure():
     hum = sensor.humidity()
     return tem, hum
 
+def setRTC(rtc, ssid, key):
+    try_connect(ssid, key)
+    try:
+        r = urequests.get(url)
+        json = ujson.loads(r.text)
+        print(json)
+        # (year, month, day, dayOfWeek!!!, hour, minute, second, millis)
+        dayOfWeek = { 'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6, }
+        rtc.datetime((
+            json['year'],
+            json['month'],
+            json['day'],
+            dayOfWeek[json['dayOfWeek']],
+            json['hour'],
+            json['minute'],
+            json['seconds'],
+            json['milliSeconds']
+        ))
+    except:
+        pass
+
+def oled_show(oled, text1, text2):
+    oled.fill(0)
+    oled.text(text1, 0,  0)
+    oled.text(text2, 0, 10)
+    oled.show()
+
 ########
 # BODY #
 ########
 i2c = machine.SoftI2C(scl = machine.Pin(5), sda = machine.Pin(4))
-oled_width, oled_height = 128, 32
-#oled_width, oled_height = 128, 64
+oled_width, oled_height = 128, 32 #128, 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-do_connect(ssid, key)
+#try to find the wifi
+for ssid_try, key_try in wifi_list:
+    #try if wifi can be connected
+    oled_show(oled, "Connecting", ssid_try)
+    if try_connect(ssid_try, key_try):
+        #set the right ssid and key
+        ssid, key = ssid_try, key_try
+        #if successful, do not try other ssid
+        break
+
+#try to set the chip datetime
 rtc = machine.RTC()
-r = urequests.get(url)
-json = ujson.loads(r.text)
-# (year, month, day, dayOfWeek!!!, hour, minute, second, millis)
-dayOfWeek = { 'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6, }
-rtc.datetime((json['year'], json['month'], json['day'], dayOfWeek[json['dayOfWeek']], json['hour'], json['minute'], json['seconds'], json['milliSeconds']))
+setRTC(rtc, ssid, key)
 
 while True:
-    tem, hum = measure()
-    co2 = "-"
+    #set RTC every hour
+    if ssid:
+        #if known wifi, just use it
+        setRTC(rtc, ssid, key)
+    else:
+        #if not known wifi, try to find it
+        for ssid_try, key_try in wifi_list:
+            #try if wifi can be connected
+            if try_connect(ssid_try, key_try):
+                #set the right ssid and key
+                ssid, key = ssid_try, key_try
+                #if successful, do not try other ssid
+                break
 
-    #year, month, day, hour, minute, seconds, weekday, yearday = time.localtime()
-    #year, month, day, hour, minute, second, microsecond, tzinfo = rtc.datetime()
-    year, month, day, weekday, hour, minute, second, microsecond = rtc.datetime()
+    #for each minute in an hour, do measurements
+    for _ in range(60):
+        tem, hum = measure()
+        co2 = "-"
+        state1 = "{} oC {} %".format(tem, hum)
+        state2 = "{} ppm".format(co2)
 
-    now = "{}:{:02d} {}.{}.{}".format(hour, minute, day, month, year)
-    state1 = "{} oC {} %".format(tem, hum)
-    state2 = "{} ppm".format(co2)
-
-    oled.fill(0)
-    oled.text(now,    0,  0)
-    oled.text(state1, 0, 10)
-    oled.text(state2, 0, 20)
-    oled.show()
-    time.sleep(60)
+        oled.fill(0)
+        if ssid:
+            year, month, day, weekday, hour, minute, second, microsecond = rtc.datetime()
+            now = "{}:{:02d} {}.{}.{}".format(hour, minute, day, month, year)
+            oled.text(now,    0,  0)
+        oled.text(state1, 0, 10)
+        oled.text(state2, 0, 20)
+        oled.show()
+        time.sleep(60)
